4558087a2bd8db097bf3823f76caa06bea28d21f
Yuansheng Wang <membphis@gmail.com>
2019-05-29 22:16:43 +0800

    feature: fetched the nginx variable from `ngx.var` if the openresty not patched.

--- lib/resty/ngxvar.lua
+++ lib/resty/ngxvar.lua
@@ -4,6 +4,10 @@ local C = ffi.C
 local ffi_string = ffi.string
 local ngx_var = ngx.var
 local str_t = ffi.new("ngx_str_t[1]")
+local pcall = pcall
+
+
+local ngxvar_patched
 
 
 ffi.cdef([[
@@ -37,11 +41,22 @@ function _M.host()
 end
 
 
-return function(name)
-    local fun = _M[name]
-    if not fun then
+return function (name)
+    local method = _M[name]
+
+    if ngxvar_patched == nil and method then
+        local val
+        ngxvar_patched, val = pcall(method)
+        if ngxvar_patched then
+            return val
+        end
+
         return ngx_var[name]
     end
 
-    return (fun())
+    if not ngxvar_patched or not method then
+        return ngx_var[name]
+    end
+
+    return method()
 end
